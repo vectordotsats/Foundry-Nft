a4657efeffa9cad5831db4cf7f0dc29f6b58324e
Vectordotsats <Vector02ozo@gmail.com>
2025-02-17 14:34:23 +0100

    Mood commit

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -4,17 +4,17 @@ pragma solidity ^0.8.18;
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract BasicNft is ERC721 {
-    uint256 private s_tokenId;
+    uint256 private s_tokenCounter;
     mapping(uint256 => string) private s_tokenIdToUri;
 
     constructor() ERC721("Pets", "PTS") {
-        s_tokenId = 0;
+        s_tokenCounter = 0;
     }
 
     function mintNft(string memory _tokenUri) public {
-        s_tokenIdToUri[s_tokenId] = _tokenUri;
-        _safeMint(msg.sender, s_tokenId);
-        s_tokenId++;
+        s_tokenIdToUri[s_tokenCounter] = _tokenUri;
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
     }
 
     function tokenURI(
