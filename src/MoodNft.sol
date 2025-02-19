56cd836d4a8eec6c440cedce310e2b0c84056d50
Vectordotsats <Vector02ozo@gmail.com>
2025-02-19 01:15:14 +0100

    Initiating Mood Test

--- src/MoodNft.sol
+++ src/MoodNft.sol
@@ -3,27 +3,64 @@
 pragma solidity ^0.8.18;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
 contract MoodNft is ERC721 {
     uint256 private s_tokenCounter;
-    string private s_savSvg;
+    string private s_sadSvg;
     string private s_happySvg;
+
+    enum Mood {
+        HAPPY,
+        SAD
+    }
+    mapping(uint256 => Mood) public s_tokenIdToMood;
 
     constructor(
         string memory sadSvg,
         string memory happySvg
     ) ERC721("Mood Nft", "MN") {
         s_tokenCounter = 0;
-        s_savSvg = sadSvg;
+        s_sadSvg = sadSvg;
         s_happySvg = happySvg;
     }
 
     function mintNft() public {
         _safeMint(msg.sender, s_tokenCounter);
+        s_tokenIdToMood[s_tokenCounter] = Mood.HAPPY;
         s_tokenCounter++;
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "data:application/json;base64,";
     }
 
     function tokenURI(
         uint256 tokenId
-    ) public view override returns (string memory) {}
+    ) public view override returns (string memory) {
+        string memory imageUri;
+        if (s_tokenIdToMood[tokenId] == Mood.HAPPY) {
+            imageUri = s_happySvg;
+        } else {
+            imageUri = s_sadSvg;
+        }
+
+        return
+            string(
+                abi.encodePacked(
+                    _baseURI(),
+                    Base64.encode(
+                        bytes(
+                            abi.encodePacked(
+                                '{"name": "',
+                                name(),
+                                '", "description": "An NFT that reflects the owners mood.", "attributes": [{"trait_type": "mood", "value": 100}], "image" : "',
+                                imageUri,
+                                '"}'
+                            )
+                        )
+                    )
+                )
+            );
+    }
 }
---
+++ test/MoodTests.t.sol
@@ -0,0 +1,8 @@
+// SPDX-Liscence-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+import {MoodNft} from "../src/MoodNft.sol";
+
+contract MoodTests is Test {}
